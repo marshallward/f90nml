5d9cfe9aabc71b64e7914d42f5ba6ff6b72b345a
Marshall Ward <marshall.ward@gmail.com>
2014-03-21 23:17:47 +1100

    Vector index test

--- test/test_f90nml.py
+++ test/test_f90nml.py
@@ -13,9 +13,24 @@ class Test(unittest.TestCase):
                              'v_float': 1.0,
                              'v_complex': 1+2j,
                              'v_logical': True,
-                             'v_string': 'Hello'
+                             'v_string': 'Hello',
                             }
                           }
+
+        self.vector_nml = {'vector_nml':
+                            {'v': [1, 2, 3, 4, 5],
+                             'v_idx': [1, 2, 3, 4],
+                             'v_idx_ooo': [1, 2, 3, 4],
+                             'v_range': [1, 2, 3, 4],
+                             'v_implicit_start': [1, 2, 3, 4],
+                             'v_implicit_end': [1, 2, 3, 4],
+                             'v_implicit_all': [1, 2, 3, 4],
+                             'v_null_start': [None, 2, 3, 4],
+                             'v_null_interior': [1, 2, None, 4],
+                             'v_null_end': [1, 2, 3, None],
+                            }
+                          }
+
 
     def test_empty(self):
         test_nml = f90nml.read('empty.nml')
@@ -24,6 +39,11 @@ class Test(unittest.TestCase):
     def test_types(self):
         test_nml = f90nml.read('types.nml')
         self.assertEqual(self.types_nml, test_nml)
+
+    def test_vector(self):
+        test_nml = f90nml.read('vector.nml')
+        self.assertEqual(self.vector_nml, test_nml)
+
 
 if __name__ == '__main__':
     unittest.main()
