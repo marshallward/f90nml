3d2bf41005cf735d8de981903cac31f25902194a
Marshall Ward <marshall.ward@gmail.com>
2014-03-21 22:10:08 +1100

    Basic data type test

--- test/test_f90nml.py
+++ test/test_f90nml.py
@@ -8,10 +8,22 @@ class Test(unittest.TestCase):
 
     def setUp(self):
         self.empty_nml = {'empty_nml': {}}
+        self.types_nml = {'types_nml':
+                            {'v_integer': 1,
+                             'v_float': 1.0,
+                             'v_complex': 1+2j,
+                             'v_logical': True,
+                             'v_string': 'Hello'
+                            }
+                          }
 
     def test_empty(self):
         test_nml = f90nml.read('empty.nml')
-        self.assertTrue(self.empty_nml, test_nml)
+        self.assertEqual(self.empty_nml, test_nml)
+
+    def test_types(self):
+        test_nml = f90nml.read('types.nml')
+        self.assertEqual(self.types_nml, test_nml)
 
 if __name__ == '__main__':
     unittest.main()
