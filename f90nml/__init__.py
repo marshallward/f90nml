0492ea3b3f0489d7a51bd22447b09d3f3ba73130
Marshall Ward <marshall.ward@gmail.com>
2014-09-22 14:27:07 +1000

    __init__.py streamline

--- f90nml/__init__.py
+++ f90nml/__init__.py
@@ -6,20 +6,7 @@
    :copyright: Copyright 2014 Marshall Ward, see AUTHORS for details.
    :license: Apache License, Version 2.0, see LICENSE for details.
 """
-from f90nml import namelist
-
 __version__ = '0.8-dev'
 
-# Legacy read() wrapper
-def read(nml_fname, verbose=False):
-    """Parse a Fortran 90 namelist file and store the contents in a ``dict``.
-
-    >>> data_nml = f90nml.read('data.nml')"""
-
-    return namelist.read(nml_fname, verbose)
-
-# Legacy write() wrapper
-def write(nml, nml_fname, force=False):
-    """Output dict to a Fortran 90 namelist file."""
-
-    namelist.write(nml, nml_fname, force)
+# Legacy API
+from f90nml.namelist import read, write
