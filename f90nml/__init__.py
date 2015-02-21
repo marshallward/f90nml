db1620be2a621310ca3a10c4402c7fdc38d0ce87
Marshall Ward <git@marshallward.org>
2015-02-21 16:49:03 +1100

    Removed header

--- f90nml/__init__.py
+++ f90nml/__init__.py
@@ -18,11 +18,11 @@ def read(nml_fname):
     return Parser().read(nml_fname)
 
 
-def write(nml, nml_fname, force=False, header=False):
+def write(nml, nml_fname, force=False):
     """Output namelist (nml) to a Fortran 90 namelist file (data.nml).
 
     >>> f90nml.write(nml, 'data.nml')"""
-    nml.write(nml_fname, force=force, header=header)
+    nml.write(nml_fname, force=force)
 
 
 def patch(nml_fname, nml_patch, out_fname=None):
