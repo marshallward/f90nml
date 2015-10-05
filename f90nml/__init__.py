780daefd552e60b663dce0b2ff0ba92c75130ede
Marshall Ward <marshall.ward@gmail.com>
2015-10-06 10:57:41 +1100

    Version update

--- f90nml/__init__.py
+++ f90nml/__init__.py
@@ -9,7 +9,7 @@
 from f90nml.parser import Parser
 from f90nml.namelist import Namelist
 
-__version__ = '0.17-dev'
+__version__ = '0.17'
 
 
 def read(nml_path, row_major=None, strict_logical=None):
