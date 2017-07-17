4363df82f8e77ffbf56a4ecc2d0c24c891752a18
Marshall Ward <marshall.ward@gmail.com>
2017-07-17 13:00:12 +1000

    Mininal JSON to Namelist conversion
    Command line now allows either a JSON or nml input file, and outputs a
    namelist to either a file or stdout.  (File not yet tested though.)

--- f90nml/cli.py
+++ f90nml/cli.py
@@ -5,6 +5,8 @@
 """
 
 import argparse
+import json
+import os
 import sys
 
 import f90nml
@@ -15,8 +17,40 @@ def parse():
     parser.add_argument('--version', action='version',
                         version='f90nml {}'.format(f90nml.__version__))
 
+    parser.add_argument('input')
+    parser.add_argument('output', nargs='?')
+
     if len(sys.argv) == 1:
         parser.print_help()
+        sys.exit()
+
+    args = parser.parse_args()
+
+    input_fname = args.input
+    output_fname = args.output
+
+    # Input config
+
+    if input_fname:
+        input_root, input_ext = os.path.splitext(input_fname)
+        if input_ext == '.json':
+            with open(input_fname) as input_file:
+                input_data = json.load(input_file)
+        else:
+            # Assume unrecognised extensions are namelists
+            input_data = f90nml.read(input_fname)
     else:
-        args = parser.parse_args()
-        print(args)
+        input_data = {}
+
+    # Target output
+
+    if output_fname:
+        output_root, output_ext = os.path.splitext(output_fname)
+
+        if output_ext == '.nml':
+            f90nml.write(input_data, output_fname)
+        else:
+            # TODO
+            pass
+    else:
+        f90nml.write(input_data, sys.stdout)
