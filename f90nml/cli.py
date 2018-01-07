02bea8f49569d318a2612f3ba3ee29c237ce38c3
Marshall Ward <marshall.ward@gmail.com>
2018-01-07 14:50:13 +1100

    future print() for Python 2.x

--- f90nml/cli.py
+++ f90nml/cli.py
@@ -3,6 +3,7 @@
 :copyright: Copyright 2017 Marshall Ward, see AUTHORS for details.
 :license: Apache License, Version 2.0, see LICENSE for details
 """
+from __future__ import print_function
 
 import argparse
 import io
