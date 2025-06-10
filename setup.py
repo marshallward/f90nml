aaea7440342342e2f9c2968c7076baeaa78b0790
Marshall Ward <marshall.ward@noaa.gov>
2025-06-10 15:08:50 -0400

    setup.py: Move content to setup.cfg
    This moves most of the content in setup.py to setup.cfg.
    
    Only the project name and the version parser are left in setup.py.
    
    Not really sure about the whole classifiers thing but we'll come back to
    it.
    
    Added a make rule to test the package (unsure about the name)

--- setup.py
+++ setup.py
@@ -39,48 +39,6 @@ with open(os.path.join(project_name, 'version.py'), 'rb') as version_py:
             project_version = eval(next_str)
             break
 
-# README
-project_readme_fname = 'README.rst'
-with open(project_readme_fname) as f:
-    project_readme = f.read()
-
 
 # Project setup
-setup(
-    name=project_name,
-    version=project_version,
-    description='Fortran 90 namelist parser',
-    long_description=project_readme,
-    author='Marshall Ward',
-    author_email='f90nml@marshallward.org',
-    url='http://github.com/marshallward/f90nml',
-
-    packages=['f90nml'],
-
-    entry_points={
-        'console_scripts': [
-            'f90nml = f90nml.cli:parse'
-        ]
-    },
-
-    extras_require={
-        'yaml': ['PyYAML'],
-    },
-
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Utilities',
-    ]
-)
+setup(name=project_name, version=project_version)
